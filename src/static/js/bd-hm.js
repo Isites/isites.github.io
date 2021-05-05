46370e4afa0d2cbc55804999258cc3d058000b16
wxu <23shuaixw@gmail.com>
2021-05-05 18:07:10 +0800

    rebuilding site 2021年 5月 5日 星期三 18时07分10秒 CST

---
+++ src/static/js/bd-hm.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f23a7a142c551e7218d6223a0812c5ae";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
