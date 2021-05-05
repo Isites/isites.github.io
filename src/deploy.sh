53e84a201a4099bd418b613f726d330560701225
wxu <23shuaixw@gmail.com>
2021-05-05 18:13:13 +0800

    rebuilding site 2021年 5月 5日 星期三 18时13分13秒 CST

--- src/deploy.sh
+++ src/deploy.sh
@@ -8,8 +8,10 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project, and output to ../
 /opt/repos/GO_REPO/bin/hugo -t timeline -D # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-rm -rf ../categories ../fonts ../tags ../timeline
+rm -rf ../categories ../fonts ../tags ../timeline ../js
 mv public/* ../
+
+rm -rf public
 
 cd ..
 
